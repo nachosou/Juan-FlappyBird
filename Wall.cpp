2775b631c6429b8b3c381483009417a146c145a4
juandigilio <juanignaciodigilio@gmail.com>
2023-11-06 17:43:36 -0300

    Walls generation modified

--- Wall.cpp
+++ Wall.cpp
@@ -14,7 +14,6 @@ void LoadWalls(Wall* walls)
         walls[i].position.y = static_cast<float>(GetRandomValue(0, screenHeight - walls[i].texture.height));
         walls[i].source = { 0, 0, static_cast<float>(walls[i].texture.width), static_cast<float>(walls[i].texture.height) };
         walls[i].speed.x = 90.0f;
-
     }
 }
 
@@ -29,8 +28,6 @@ static void MoveWalls(Wall* walls)
             if (walls[i].position.x + (walls[i].texture.width / 2.0f) < 0.0f)
             {
                 walls[i].position.x = static_cast<float>(screenWidth);
-
-                walls[i].position.y = static_cast<float>(GetRandomValue(0, screenHeight - walls[i].texture.height));
             }
             else if (walls[i].position.y + (walls[i].texture.height / 2.0f) > screenHeight)
             {
@@ -55,8 +52,30 @@ static void GenerateWalls(Wall* walls)
             if (!walls[i].isAlive)
             {
                 walls[i].isAlive = true;
-
                 lastWall = GetTime();
+
+                if (i == 0)
+                {
+                    if (walls[totalWalls - 1].GetCenter().y < screenHeight / 2.0f)
+                    {
+                        walls[i].position.y = static_cast<float>(GetRandomValue(static_cast<int>(walls[totalWalls - 1].GetCenter().y), static_cast<int>(walls[totalWalls - 1].position.y + walls[totalWalls - 1].texture.height)));
+                    }
+                    else
+                    {
+                        walls[i].position.y = static_cast<float>(GetRandomValue(static_cast<int>(walls[totalWalls - 1].GetCenter().y), static_cast<int>(walls[totalWalls - 1].position.y - walls[totalWalls - 1].texture.height)));
+                    }
+                }
+                else
+                {
+                    if (walls[i - 1].GetCenter().y < screenHeight / 2.0f)
+                    {
+                        walls[i].position.y = static_cast<float>(GetRandomValue(static_cast<int>(walls[i - 1].GetCenter().y), static_cast<int>(walls[i - 1].position.y + walls[i - 1].texture.height)));
+                    }
+                    else
+                    {
+                        walls[i].position.y = static_cast<float>(GetRandomValue(static_cast<int>(walls[i - 1].GetCenter().y), static_cast<int>(walls[i - 1].position.y - walls[i - 1].texture.height)));
+                    }
+                }
 
                 break;
             }
