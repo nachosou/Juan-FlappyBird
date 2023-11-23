81df2a9b352dffa0a5ee90a80b932e3bf788ff68
nachosou <nachosou123@gmail.com>
2023-11-22 22:13:10 -0300

    Fix .bat

--- utils/assetsCopy.bat
+++ utils/assetsCopy.bat
@@ -2,6 +2,21 @@
 
 echo Copying asstets
 
-xcopy /y /i /s "$(ProjectDir)Assets" "$(OutDir)Assets"
+set dirToCopy=%~1
+set targetDir=%~2
+
+echo Copying
+
+echo %dirToCopy%
+echo %targetDir%
+
+cd "%targetDir%"
+mkdir "Assets"
+xcopy /y /s /e "%dirToCopy%" "%targetDir%/Assets"
+
+pause
+REM cd "$(OutDir)"
+REM mkdir "Assets"
+REM xcopy /s /e /y "$(SolutionDir)Assets" "$(OutDir)Assets"
 
 echo Assets copied successfully
